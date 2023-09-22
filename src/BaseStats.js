5e07002028249b10dab5bc561978d4df24705b6a
Stephany K <stephanykleen@gmail.com>
2023-09-22 19:18:47 +0000

    Working with props to pass data objects

---
+++ src/BaseStats.js
@@ -0,0 +1,29 @@
+import "./BaseStats.css";
+
+function BaseStats({ clicker, stats: { hp, attack, defense, speed } }) {
+  return (
+    <div className="base-stats">
+      <h1>BaseStats</h1>
+      <button onClick={clicker}>Check Special Stats</button>
+      <table>
+        <tr>
+          <td>Hit Points</td>
+          <td>{hp}</td>
+        </tr>
+        <tr>
+          <td> Attack</td>
+          <td>{attack}</td>
+        </tr>
+        <tr>
+          <td>Defense</td>
+          <td>{defense}</td>
+        </tr>
+        <tr>
+          <td>Speed</td>
+          <td>{speed}</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
+export default BaseStats;
